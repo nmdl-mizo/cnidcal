3eaf8fa2ef30d0de1893201a368f8a5b621c683d
Jason Xie <48645456+jinlhr542@users.noreply.github.com>
2021-07-08 03:31:56 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="cnidcal",
+    version="0.0.1",
+    license="MIT",
+    author="Yaoshu Xie",
+    author_email="ysxie@iis.u-tokyo.ac.jp",
+    install_requires=["numpy"],
+)
