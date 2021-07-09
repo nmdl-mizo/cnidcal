5200d8c8c6c557f0fb8db7a5b4d3b47de305389c
Jason Xie <48645456+jinlhr542@users.noreply.github.com>
2021-07-09 17:49:06 +0900

    fix the setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup
+import setuptools
 
-setup(
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setuptools.setup(
     name="cnidcal",
     version="0.0.1",
     license="MIT",
     author="Yaoshu Xie",
     author_email="ysxie@iis.u-tokyo.ac.jp",
+    packages=setuptools.find_packages(),
     install_requires=["numpy", "matplotlib"],
 )
