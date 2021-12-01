0a5bf38aab47646628e2699110e250ac7d72d424
Jason Xie <48645456+jinlhr542@users.noreply.github.com>
2021-12-01 12:59:02 +0900

    save after legend

--- cnidcal/celldraw.py
+++ cnidcal/celldraw.py
@@ -82,7 +82,7 @@ class cellsdrawer:
         
     def draw_direct(self, xlow, xhigh, ylow, yhigh, figsize_x, figsize_y, show_CSL = True, \
     show_CNID_points = False, show_CNID_cell = False, show_lattice_1 = True, show_lattice_2 = True, size_LP_1 = 150, \
-    size_LP_2 = 50, save = False, dpi = 600, show_axis = False, lim = 50, show_legend = False, filename = 'Figure'):
+    size_LP_2 = 50, save = False, dpi = 600, show_axis = False, lim = 50, show_legend = False):
         """
         A function drawing the direct lattices
         """
@@ -120,14 +120,14 @@ class cellsdrawer:
         plt.ylim(ylow,yhigh)
         if show_axis == False:
             plt.axis('off')
-        if save == True:
-            plt.savefig(filename, dpi = 600, format = 'jpg')
         if show_legend == True:
         	  plt.legend()
+        if save == True:
+            plt.savefig('direct_cells.png',dpi = 600, format = 'png')
     
     def draw_reciprocal(self, xlow, xhigh, ylow, yhigh, figsize_x, figsize_y, show_CSL_R = True, \
     show_lattice_1_R = True, show_lattice_2_R = True, size_LP_1_R = 150, \
-    size_LP_2_R = 50, save = False, dpi = 600, show_axis = False, lim = 50, show_legend = False, filename = 'Figure'):
+    size_LP_2_R = 50, save = False, dpi = 600, show_axis = False, lim = 50, show_legend = False):
         """
         A function drawing the direct lattices
         """
@@ -160,7 +160,7 @@ class cellsdrawer:
         plt.ylim(ylow,yhigh)
         if show_axis == False:
             plt.axis('off')
-        if save == True:
-            plt.savefig(filename, dpi = 600, format = 'jpg')
         if show_legend == True:
-        	  plt.legend()        
+        	  plt.legend()       
+        if save == True:
+            plt.savefig('reciprocal_cells.png',dpi = 600, format = 'png')        	   
